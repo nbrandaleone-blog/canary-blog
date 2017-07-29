ed0cd67cf5339a4d02d8e4c6686621672a8b1754
Nick Brandaleone <nbrand@mac.com>
2017-07-29 18:19:51 -0400

    Removing old test files and sample code

--- scripts/create_table.sh
+++ scripts/create_table.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-aws dynamodb create-table \
---table-name CanaryTable \
---attribute-definitions AttributeName=NewContainerName,AttributeType=S \
---key-schema AttributeName=NewContainerName,KeyType=HASH \
---provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
-
-sleep 10 
-
 aws dynamodb put-item --table-name CanaryTable \
 --item '{
 	"NewContainerName":{"S":"green-app"},
+	"NewLB":{"S":"dummy"},
+	"OldLB":{"S":"dummy"},
+	"Triggered":{"BOOL":false},
+	"HostedZoneID":{"S":"dummy"},
+	"RecordName":{"S":"dummy"},
+	"LBZoneID":{"S":"dummy"} }' \
+--return-consumed-capacity TOTAL
+
+aws dynamodb put-item --table-name CanaryTable \
+--item '{
+	"NewContainerName":{"S":"new-container-service"},
 	"NewLB":{"S":"dummy"},
 	"OldLB":{"S":"dummy"},
 	"Triggered":{"BOOL":false},
